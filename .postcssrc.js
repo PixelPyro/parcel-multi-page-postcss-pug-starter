51d2274e91c1bc40215e8f680eea530f2cedb126
Umar <umr.farq@gmail.com>
2018-08-16 00:21:32 +0100

    Cleanup :+1:

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,13 +1,13 @@
 module.exports = {
   map: false,
   plugins: {
+    "postcss-import": {},
+    "postcss-simple-vars": {},
+    "postcss-nested": {},
     "postcss-plugin": {},
     "postcss-utilities": {},
     "postcss-mixins": {},
-    "postcss-simple-vars": {},
-    "postcss-nested": {},
     "autoprefixer": {},
-    "postcss-import": {},
     "postcss-circle": {},
     "rucksack-css": {},
     "cssnano": {},
